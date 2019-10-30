6900f382ee409651441eadb01b634f6b3e8eeee1
doompriest <unslaadkrosis@rambler.ru>
2019-10-30 12:51:38 +0700

    Small code chacnges thanks to almighty Rubocop

--- valera_checker.rb
+++ valera_checker.rb
@@ -1,21 +1,20 @@
+# TODO: handing w/o object creation (self?)
+# TODO: private
+
 # Checker for available actions
-#TODO handing w/o object creation (self?)
-#TODO private
-
 class ValeraChecker
-
   def check(valera, action_string)
     action = validate_action(action_string)
-    action = nil unless (available?(valera, action) && valera.alive?)
+    action = nil unless available?(valera, action) && valera.alive?
     action
   end
 
   def validate_action(action_string)
-    available_actions = ['work', 'behold', 'wine', 'bar', \
-                         'marginal', 'sing', 'sleep']
+    available_actions = %w[work behold wine bar \
+                           marginal sing sleep]
     (available_actions.include? action_string) ? action_string : nil
   end
-  
+
   def available?(valera, action)
     action_available = true
     action_available = false \
