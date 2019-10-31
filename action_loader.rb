fce93cd502083857a7ba031a94bab22818c8837f
SazPavel <sp15sp@yandex.ru>
2019-10-31 14:20:06 +0700

    add action loader

---
+++ action_loader.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'yaml'
+require './actions_container.rb'
+# Load all action from file
+class ActionLoader
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def load
+    ActionsContainer.new(
+      YAML.safe_load(File.open(@filename)).map { |data| build_action(data) }
+    )
+  end
+
+  private
+
+  def build_action(data)
+    Action.new name: data['name'],
+               after_text: data['after_text'],
+               before_text: data['before_text'],
+               conds: data['conds'].map { |cond| build_cond(cond) },
+               effects: data['effects'].map { |effect| build_effect(effect) }
+  end
+
+  # TODO
+  def build_cond(data) end
+
+  def build_effect(data) end
+end
