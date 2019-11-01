c609e100a78689dd75369d498d7c05b64ae0f1db
SazPavel <sp15sp@yandex.ru>
2019-11-01 11:18:58 +0700

    rubocop

--- action.rb
+++ action.rb
@@ -5,7 +5,8 @@
 # Stored all informations about action
 class Action
   attr_accessor :name, :conds, :effects, :before_text, :after_text
-  def initialize(name: '', after_text: '', before_text: '', conds: [], effects: [])
+  def initialize(name: '', after_text: '',
+                 before_text: '', conds: [], effects: [])
     @name = name
     @before_text = before_text
     @after_text = after_text
